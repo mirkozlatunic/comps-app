d7e965d00a6880bb4ddd369860619e6978bb416d
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-05-25 19:39:50 -0700

    button component setup with props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import Button from "./Button";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button>Click Me</Button>
+      </div>
+      <div>
+        <Button>Buy Now</Button>
+      </div>
+      <div>
+        <Button>See Deal</Button>
+      </div>
+      <div>
+        <Button>Hide Ads</Button>
+      </div>
+      <div>
+        <Button>Warning</Button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
