4a826592caeb57f01de17cf07f5f4ce57787bef4
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-05-30 22:05:01 -0700

    navigation for provider completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-
 const App = () => {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Blue", value: "blue" },
-    { label: "Green", value: "green" },
-  ];
-  return (
-    <div className="flex">
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
